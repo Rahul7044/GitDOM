3582c8cc8dd1a4b1dc0456ecb629f9fcad1aa656
Rahul7044 <rahulkumar581999@gmail.com>
2023-01-29 17:23:43 +0530

    secondfile created

---
+++ demo.js
@@ -0,0 +1,8 @@
+var headerTitle=document.getElementById('header-title');
+//var header=document.getElementById('main-header');
+//headerTitle.innerHTML='<h3>Hello</h3>'
+//header.style.border='solid 3px #000';
+document.getElementById('header-title').style.border = " solid 3px black";
+//hTitle.style.fontWeight='bold';
+document.getElementById('title-h2').style.color = "green";
+document.getElementById('title-h2').style.fontStyle = "bold";
